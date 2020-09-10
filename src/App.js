86b5e8ec2a87b2f5039e1270d846433891f95fb3
Rafael Fernandes <rafaelvbf3@hotmail.com>
2020-09-10 15:54:05 -0300

    Update files
    updating

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+//imports
+import React, { useState, useEffect } from 'react';
+import "./styles.css";
+import api from './services/api'
+
+
+function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then(response => setRepositories(response.data))
+  }, [])
+
+  async function handleAddRepository() {
+    const response = await api.post('/repositories', {
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+    })
+    const repository = response.data
+    setRepositories([...repositories, repository])
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`/repositories/${id}`)
+    const repositoriesNew = repositories.filter(repository => repository.id !== id)
+    setRepositories(repositoriesNew)
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository =>
+          <li key={repository.id}
+          >{repository.title}
+            <button className="delete" onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>)}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
